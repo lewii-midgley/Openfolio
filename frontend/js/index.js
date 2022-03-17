32456899f0ce1cd40cac96486e59a4350ce3d08b
jamespfawcett <james.p.fawcett@gmail.com>
2022-03-17 16:41:29 +1300

    working on hover effect of all cards

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -22,18 +22,42 @@ $(document).ready(function(){
                 let i;
                 document.getElementById('mainGrid').innerHTML = '';
                 for(i = 0; i < projectsFromMongo.length; i++){
-                    console.log(projectsFromMongo[i]);
+                   
+                    let projectName = projectsFromMongo[i].name;
+                    console.log(projectName);
+                    
                     document.getElementById('mainGrid').innerHTML +=
-                    `<div class="card">
-                    <div class="card__top">
-                        <i class="card__icon icon fa-solid fa-pen"></i>
-                        <i class="card__icon icon fa-solid fa-trash"></i>
-                    </div>
-                    <div class="card__bottom">
-                        <h2 class="card__title">${projectsFromMongo[i].name}</h2>
-                        <h3 class="card__author">${projectsFromMongo[i].author}</h3>
-                    </div>
-                </div>`
+                    `<div id="${projectsFromMongo[i]._id}" class="card">
+                    
+                        <div class="card__top">
+                            <i class="hide card__icon icon fa-solid fa-pen"></i>
+                            <i class="hide card__icon icon fa-solid fa-trash"></i>
+                        </div>
+                        <div class="card__bottom">
+                            <h2 class="hide card__title">${projectsFromMongo[i].name}</h2>
+                            <h3 class="hide card__author">${projectsFromMongo[i].author}</h3>
+                        </div>
+                        
+                    </div>`
+                    
+
+                   $('.card').mouseover(function(e){
+                       const hides = e.target.querySelectorAll('.hide');
+                       for(const hide of hides){
+                        hide.classList.remove('hide');
+                        hide.classList.add('show');
+                        console.log('mouseover');
+                       }
+                    });
+                    $('.card').mouseout(function(e){
+                        const hides = e.target.querySelectorAll('.show');
+                        for(const hide of hides){
+                         hide.classList.remove('show');
+                         hide.classList.add('hide');
+                         console.log('mouseout');
+                        }
+                     });
+                    
                 }
             },//success
             error:function(){
@@ -42,11 +66,12 @@ $(document).ready(function(){
         })//ajax
     })//view
 
+// Card Hover
 
 
 
-
-
+{/* <h2 class="card__title">${projectsFromMongo[i].name}</h2>
+<h3 class="card__author">${projectsFromMongo[i].author}</h3> */}
 
 
 
